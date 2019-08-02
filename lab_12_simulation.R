673a1efa269aad48720d332aa19be8721182c576
Suraj Nair <Suraj@Surajs-MacBook-Pro.local>
2019-08-02 10:23:09 -0400

    2b commit

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,3 +8,15 @@ generate_data <- function(n, p){
   
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select <- function(covariates, responses, cutoff){
+  lin_reg_1 <- lm(responses ~ covariates)
+  
+  indices <- which(lin_reg_1$p.value <= cutoff)
+  
+  if (length(indices) == 0) return(c())
+  
+  lin_reg_2 <- lm(responses ~ covariates[,indices])
+  
+  return(lin_reg_2$p.value)
+}
